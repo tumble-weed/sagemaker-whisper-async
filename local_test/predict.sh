b6aee1484ab1e6c21d3ad2f9ad4ff215ab2e5eb8
aniket <aniketsinghresearch@gmail.com>
2022-12-04 13:53:37 +0000

    POST protocol

--- local_test/predict.sh
+++ local_test/predict.sh
@@ -4,4 +4,4 @@ url=$1
 payload=$2
 content=${3:-text/csv}
 
-curl --data-binary @${payload} -H "Content-Type: ${content}" -v ${url}/invocations
+curl --data-binary @${payload} -H "Content-Type: ${content}" -v ${url}/invocations -X POST
