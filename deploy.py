167f1b3ba8eefb41da9c2b0d298f9675231ef8df
aniket <aniketsinghresearch@gmail.com>
2022-12-06 05:30:44 +0000

    autoscaling flag

--- deploy.py
+++ deploy.py
@@ -70,47 +70,48 @@ def main(args):
                                                  async_inference_config=AsyncInferenceConfig(output_path = f"s3://sagemaker-ap-northeast-1-268945887964/{args.model_prefix}/response",max_concurrent_invocations_per_instance=1)
                         )
     #############################################################################
-    # autoscaling
-    client = boto3.client(
-        "application-autoscaling"
-    )  # Common class representing Application Auto Scaling for SageMaker amongst other services
+#     # autoscaling
+    if args.autoscaling:
+        client = boto3.client(
+            "application-autoscaling"
+        )  # Common class representing Application Auto Scaling for SageMaker amongst other services
 
-    resource_id = (
-        "endpoint/" + endpoint_name + "/variant/" + "AllTraffic"
-    )  # This is the format in which application autoscaling references the endpoint
+        resource_id = (
+            "endpoint/" + endpoint_name + "/variant/" + "AllTraffic"
+        )  # This is the format in which application autoscaling references the endpoint
 
-    # Configure Autoscaling on asynchronous endpoint down to zero instances
-    response = client.register_scalable_target(
-        ServiceNamespace="sagemaker",
-        ResourceId=resource_id,
-        ScalableDimension="sagemaker:variant:DesiredInstanceCount",
-        MinCapacity=0,
-        MaxCapacity=5,
-    )
+        # Configure Autoscaling on asynchronous endpoint down to zero instances
+        response = client.register_scalable_target(
+            ServiceNamespace="sagemaker",
+            ResourceId=resource_id,
+            ScalableDimension="sagemaker:variant:DesiredInstanceCount",
+            MinCapacity=0,
+            MaxCapacity=5,
+        )
 
-    response = client.put_scaling_policy(
-        PolicyName="Invocations-ScalingPolicy",
-        ServiceNamespace="sagemaker",  # The namespace of the AWS service that provides the resource.
-        ResourceId=resource_id,  # Endpoint name
-        ScalableDimension="sagemaker:variant:DesiredInstanceCount",  # SageMaker supports only Instance Count
-        PolicyType="TargetTrackingScaling",  # 'StepScaling'|'TargetTrackingScaling'
-        TargetTrackingScalingPolicyConfiguration={
-            "TargetValue": 5.0,  # The target value for the metric. - here the metric is - SageMakerVariantInvocationsPerInstance
-            "CustomizedMetricSpecification": {
-                "MetricName": "ApproximateBacklogSizePerInstance",
-                "Namespace": "AWS/SageMaker",
-                "Dimensions": [{"Name": "EndpointName", "Value": endpoint_name}],
-                "Statistic": "Average",
+        response = client.put_scaling_policy(
+            PolicyName="Invocations-ScalingPolicy",
+            ServiceNamespace="sagemaker",  # The namespace of the AWS service that provides the resource.
+            ResourceId=resource_id,  # Endpoint name
+            ScalableDimension="sagemaker:variant:DesiredInstanceCount",  # SageMaker supports only Instance Count
+            PolicyType="TargetTrackingScaling",  # 'StepScaling'|'TargetTrackingScaling'
+            TargetTrackingScalingPolicyConfiguration={
+                "TargetValue": 5.0,  # The target value for the metric. - here the metric is - SageMakerVariantInvocationsPerInstance
+                "CustomizedMetricSpecification": {
+                    "MetricName": "ApproximateBacklogSizePerInstance",
+                    "Namespace": "AWS/SageMaker",
+                    "Dimensions": [{"Name": "EndpointName", "Value": endpoint_name}],
+                    "Statistic": "Average",
+                },
+                "ScaleInCooldown": 600,  # The cooldown period helps you prevent your Auto Scaling group from launching or terminating
+                # additional instances before the effects of previous activities are visible.
+                # You can configure the length of time based on your instance startup time or other application needs.
+                # ScaleInCooldown - The amount of time, in seconds, after a scale in activity completes before another scale in activity can start.
+                "ScaleOutCooldown": 300  # ScaleOutCooldown - The amount of time, in seconds, after a scale out activity completes before another scale out activity can start.
+                # 'DisableScaleIn': True|False - ndicates whether scale in by the target tracking policy is disabled.
+                # If the value is true , scale in is disabled and the target tracking policy won't remove capacity from the scalable resource.
             },
-            "ScaleInCooldown": 600,  # The cooldown period helps you prevent your Auto Scaling group from launching or terminating
-            # additional instances before the effects of previous activities are visible.
-            # You can configure the length of time based on your instance startup time or other application needs.
-            # ScaleInCooldown - The amount of time, in seconds, after a scale in activity completes before another scale in activity can start.
-            "ScaleOutCooldown": 300  # ScaleOutCooldown - The amount of time, in seconds, after a scale out activity completes before another scale out activity can start.
-            # 'DisableScaleIn': True|False - ndicates whether scale in by the target tracking policy is disabled.
-            # If the value is true , scale in is disabled and the target tracking policy won't remove capacity from the scalable resource.
-        },
-    )
+        )
 
     #############################################################################
 
@@ -130,5 +131,6 @@ if __name__ == '__main__':
     parser.add_argument('--train_instance_type',type=str,default='ml.m4.xlarge')
     parser.add_argument('--instance_type',type=str,default='ml.t2.medium')
     parser.add_argument('--model_prefix',type=str,default='dummy-whisper1')
+    parser.add_argument('--autoscaling',type=lambda s:(s.lower() == 'true'),default=False)
     args = parser.parse_args()
     main(args)
