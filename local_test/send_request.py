3ebda74fd57cbb914f17f9a7a7babb0b07a0b5c0
aniket <aniketsinghresearch@gmail.com>
2022-12-13 03:37:12 +0000

    client to check docker container

--- local_test/send_request.py
+++ local_test/send_request.py
@@ -2,7 +2,9 @@ import json
 import os
 import tempfile
 import base64
+from timeit import default_timer as timer
 def send_request(audio_path,url):
+    print('using',audio_path)
     #=====================================================================
     audio_base64 = base64.b64encode(open(audio_path, "rb").read())
     ext = audio_path.split('.')[-1]
@@ -17,7 +19,11 @@ def send_request(audio_path,url):
         os.system(f'bash predict.sh {url} {f.name} "application/json"')
 
 # audio_path = '/home/ec2-user/SageMaker/Audio_James_Randi.wav'
-audio_path = '/home/ec2-user/SageMaker/Audio_James_Randi_Long_Version.ogg'
+# audio_path = '/home/ec2-user/SageMaker/Audio_James_Randi_Long_Version.ogg'
+audio_path = '/home/ec2-user/SageMaker/thecantervilleghostversion2_01_wilde_128kb.mp3'
+start = timer()
 send_request(audio_path,'127.0.0.1:5000')
 
-# docker run -p 5000:80 --device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidia-caps:/dev/nvidia-caps --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm --device /dev/nvidia-uvm-tools:/dev/nvidia-uvm-tools --name localserve -it dummy-whisper-async:latest bash
+# your code here    
+print(timer() - start)
+
